8215678b76f2f8f4c49faecdcb9c9376afdaaa52
William Koller <williamkoller404@gmail.com>
2022-03-20 21:15:03 -0300

    feat: add examples

---
+++ Hour.java
@@ -0,0 +1,12 @@
+public class Hour {
+  private String currentTime;
+
+  public Hour(String hour) {
+    super();
+    this.currentTime = hour;
+  }
+
+  public String toString() {
+    return this.currentTime;
+  }
+}
---
+++ Ubuntu.java
@@ -0,0 +1,5 @@
+public class Ubuntu {
+  public static void main(String args[]) {
+    System.out.println("Java Unix");
+  }
+}
